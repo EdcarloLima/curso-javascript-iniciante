8b4d36ae7b71245576f4969da46b6710fdea94d1
Edcarlo Lima <edcarlolimsan@gmail.com>
2021-08-12 22:50:06 -0300

    Primeira aula - introduçção - usando alert console.log replace toUpperCase toLowerCase

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//alert('alert - Programando js em arquivo separado');
+
+var nome = 'Edcarlo Lima';
+var idade = 30;
+var tempo = 10;
+//alert('Bem-vindo');
+//alert(nome+' tem '+idade+' somando + tempo = ');
+//alert(idade+tempo);
+
+// f12 ou botão direito do mouse inspecionar
+console.log('Exibindo no console '+nome+' tem '+idade);
+
+// substituição de caracteres em string
+var texto = 'O melhor país é o Japão.';
+console.log(texto.replace('Japão','Brasil'));
+// Transformando tudo em maiúsculo
+console.log(texto.toUpperCase());
+// Transformando tudo em minúsculo
+console.log(texto.toLowerCase());
+// Multiplicando
+console.log('Multiplicando idade x tempo ');
+console.log(30*10);
